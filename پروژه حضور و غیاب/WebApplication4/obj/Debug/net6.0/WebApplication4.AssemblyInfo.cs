93a7beeb7efefd635def3aa26d1be9688149909a
mohammad <m.reza13831339@gmail.com>
2025-05-28 15:28:38 +0330

    0.2

--- پروژه حضور و غیاب/WebApplication4/obj/Debug/net6.0/WebApplication4.AssemblyInfo.cs
+++ پروژه حضور و غیاب/WebApplication4/obj/Debug/net6.0/WebApplication4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03736ed731624404985de3557c636facd7830abb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
